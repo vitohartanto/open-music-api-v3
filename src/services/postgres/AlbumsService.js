eb8a097a88bd97146fe1756ac4f547e480aa359d
Vito <vitohartanto@mail.ugm.ac.id>
2024-09-27 12:32:26 +0700

    AlbumsService.js done

--- src/services/postgres/AlbumsService.js
+++ src/services/postgres/AlbumsService.js
@@ -1,12 +1,77 @@
 const { Pool } = require('pg');
-const { nanoid } = require('nanoid')
+const { nanoid } = require('nanoid');
 
 class AlbumsService {
   constructor() {
     this._pool = new Pool();
   }
 
-  async addAlbum = ({ name, year }) => {
+  addAlbum = async ({ name, year }) => {
     const id = nanoid(16);
-  }
+    const createdAt = new Date().toISOString();
+    const updatedAt = createdAt;
+
+    const query = {
+      text: 'INSERT INTO albums VALUES($1 $2 $3 $4 $5) RETURNING id',
+      values: [id, name, year, createdAt, updatedAt],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows[0].id) {
+      throw new InvariantError('Album gagal ditambahkan');
+    }
+
+    return result.rows[0].id;
+  };
+
+  getAlbums = async () => {
+    const result = await this._pool.query('SELECT * FROM albums');
+    return result.rows.map(mapDBToModel);
+  };
+
+  getAlbumById = async (id) => {
+    const query = {
+      text: 'SELECT * FROM albums WHERE id = $1',
+      values: [id],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new NotFoundError('Catatan tidak ditemukan');
+    }
+
+    return result.rows.map(mapDBToModel)[0];
+  };
+
+  editAlbumById = async (id, { name, year }) => {
+    const updatedAt = new Date().toISOString();
+
+    const query = {
+      text: 'UPDATE albums SET name = $1, year = $2, updated_at = $3 WHERE id = $4 RETURNING id',
+      values: [name, year, updatedAt, id],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new NotFoundError('Gagal memperbarui album. Id tidak ditemukan');
+    }
+  };
+
+  deleteAlbumById = async (id) => {
+    const query = {
+      text: 'DELETE FROM albums WHERE id = $1 RETURNING id',
+      values: [id],
+    };
+
+    const result = await this._pool.query(query);
+
+    if (!result.rows.length) {
+      throw new NotFoundError('Album gagal dihapus. Id tidak ditemukan');
+    }
+  };
 }
+
+module.exports = AlbumsService;
