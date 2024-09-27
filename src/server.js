efd66a2db1350c7ae6967ed6d5c3d35aa780340e
Vito <vitohartanto@mail.ugm.ac.id>
2024-09-27 10:17:46 +0700

    feat: attach dotenv

--- src/server.js
+++ src/server.js
@@ -1,9 +1,14 @@
+require('dotenv').config();
 const Hapi = require('@hapi/hapi');
 
 const init = async () => {
   // Membuat server di port 3000
   const server = Hapi.server({
-    port: 3000,
-    host: 'localhost',
+    port: process.env.PORT,
+    host: process.env.HOST,
   });
+
+  // Memulai Server
+  await server.start();
+  console.log('Server running on %s', server.info.uri);
 };
