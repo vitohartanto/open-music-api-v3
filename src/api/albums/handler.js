e973fab82f35ec83a21adf495530e370c68ca9c6
Vito <vitohartanto@mail.ugm.ac.id>
2024-09-27 11:54:10 +0700

    feat: handler.js

---
+++ src/api/albums/handler.js
@@ -0,0 +1,14 @@
+const autoBind = require('auto-bind');
+
+class AlbumsHandler {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+
+    autoBind(this);
+  }
+
+  //   async addAlbumHandler = (request, h) => {
+
+  //   }
+}
