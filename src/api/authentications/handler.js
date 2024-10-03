3becf7a9422107c1706db7bbd41c33b534fd9af1
Vito <vitohartanto@mail.ugm.ac.id>
2024-10-03 16:28:46 +0700

    handler.js for authentications done

--- src/api/authentications/handler.js
+++ src/api/authentications/handler.js
@@ -4,6 +4,11 @@ class AuthenticationsHandler {
     this._usersService = usersService;
     this._tokenManager = tokenManager;
     this._validator = validator;
+
+    this.postAuthenticationHandler = this.postAuthenticationHandler.bind(this);
+    this.putAuthenticationHandler = this.putAuthenticationHandler.bind(this);
+    this.deleteAuthenticationHandler =
+      this.deleteAuthenticationHandler.bind(this);
   }
 
   async postAuthenticationHandler(request, h) {
@@ -35,7 +40,7 @@ class AuthenticationsHandler {
     return response;
   }
 
-  async putAuthenticationHandler(request, h) {
+  async putAuthenticationHandler(request) {
     // Memastikan payload request mengandung properti refreshToken yang bernilai string
     this._validator.validatePutAuthenticationPayload(request.payload);
 
@@ -52,6 +57,17 @@ class AuthenticationsHandler {
       data: {
         accessToken,
       },
+    };
+  }
+
+  async deleteAuthenticationHandler(request) {
+    this._validator.validateDeleteAuthenticationPayload(request.payload);
+    const { refreshToken } = request.payload;
+    await this._authenticationsService.verifyRefreshToken(refreshToken);
+    await this._authenticationsService.deleteRefreshToken(refreshToken);
+    return {
+      status: 'success',
+      message: 'Refresh token berhasil dihapus',
     };
   }
 }
