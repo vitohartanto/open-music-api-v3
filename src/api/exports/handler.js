4c7dab1f2bd913ba666d18e7aa768f0b1b56c8d7
Vito <vitohartanto@mail.ugm.ac.id>
2024-10-08 14:17:34 +0700

    handler.js for exports

---
+++ src/api/exports/handler.js
@@ -0,0 +1,39 @@
+const autoBind = require('auto-bind');
+
+class ExportsHandler {
+  constructor(producerService, playlistsService, validator) {
+    this._producerService = producerService;
+    this._playlistsService = playlistsService;
+    this._validator = validator;
+
+    autoBind(this);
+  }
+
+  async postExportSongsHandler(request, h) {
+    this._validator.validateExportSongsPayload(request.payload);
+
+    const { id: credentialId } = request.auth.credentials;
+    const { playlistId } = request.params;
+
+    await this._playlistsService.verifyPlaylistAccess(playlistId, credentialId);
+
+    const message = {
+      playlistId,
+      targetEmail: request.payload.targetEmail,
+    };
+
+    await this._producerService.sendMessage(
+      'export:playlists',
+      JSON.stringify(message)
+    );
+
+    const response = h.response({
+      status: 'success',
+      message: 'Permintaan Anda sedang kami proses',
+    });
+    response.code(201);
+    return response;
+  }
+}
+
+module.exports = ExportsHandler;
